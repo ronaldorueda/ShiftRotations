0dfa3b425beea8bd03f986d7f0d4dda37e82bf92
Ronaldo Rueda <rueda.ronaldo@hotmail.com>
2021-06-11 16:30:52 -0500

    Updated test.py to test sending emails from python. Worked amazing. Needs to me implemented with performRotations.py. should add more to email to make it cleaner and less sussy. test files are now removed as no longer needed. sample text file is adding showing new naming scheme in play.

--- test.py
+++ test.py
@@ -1,11 +1,37 @@
-from datetime import date, timedelta
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+import email.mime.application
+import mimetypes
 
-newFileName = ""
+recipients = ['']
 
-date = date.today() + timedelta(days=3)
-datestring = date.strftime("%m-%d-%Y")
+msg = MIMEMultipart()
+msg['From'] = 'rotationsemail@gmail.com'
+msg['To'] = ", ".join(recipients)
+msg['Subject'] = 'Here is the new schedule for the following week.'
+message = 'Schedule for week of 14-06-2021'
+msg.attach(MIMEText(message))
 
-newFileName = newFileName + "test-" + datestring + ".txt"
+attach_file_name = 'Schedule for the week of 14-06-2021.txt'
+fo=open(attach_file_name,'rb')
+attach = email.mime.application.MIMEApplication(fo.read(),_subtype="txt")
+fo.close()
+attach.add_header('Content-Disposition','attachment',filename=attach_file_name)
+msg.attach(attach)
 
-with open(newFileName, 'w') as fp:
-    pass
+mailserver = smtplib.SMTP('smtp.gmail.com',587)
+# identify ourselves to smtp gmail client
+mailserver.ehlo()
+# secure our email with tls encryption
+mailserver.starttls()
+# re-identify ourselves as an encrypted connection
+mailserver.ehlo()
+mailserver.login('', '')
+
+mailserver.sendmail('rotationsemail@gmail.com',recipients,msg.as_string())
+
+mailserver.quit()
+
+print("Script complete.")
